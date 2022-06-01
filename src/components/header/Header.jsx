c5415de739bc70f1b4ddd85bc44c53cb0becc20d
noui61 <noui@metavault.org>
2022-06-01 14:03:34 +0300

    header section

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import CTA from './CTA';
 import './header.css';
+import ME from '../../assets/me.png'
+import HeaderSocials from './HeaderSocials';
 
 const Header = () => {
   return (
-    <div>
-      Header
-    </div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>MURAT AKTAŞ</h1>
+        <h5 className="text-light">Front-end Developer</h5>
+        <CTA/>
+        <HeaderSocials/>
+        <div className='me'>
+          <img src={ME} alt='' />
+        </div>
+
+        <a href='#contact' className='scroll_down'>Scroll Down</a>
+      </div>
+    </header>
   )
 }
 
---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {BsGithub,BsLinkedin} from 'react-icons/bs'
+const HeaderSocials = () => {
+  return (
+    <div className='header__socials'>
+      <a href='' target="_blank"><BsLinkedin/></a>
+      <a href='' target="_blank"><BsGithub/></a>
+    </div>
+  )
+}
+
+export default HeaderSocials
