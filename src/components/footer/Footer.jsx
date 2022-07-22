0ca56e0090cf1d93446a8db4451f83ea14e01752
noui61 <noui@metavault.org>
2022-07-22 16:15:15 +0300

    footer update

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import './footer.css'
+import {FaFacebookF,FaInstagramSquare,FaLinkedin} from 'react-icons/fa'
 
 const Footer = () => {
   return (
-    <div>
-      footer
-    </div>
+    <footer>
+      <a href='#' className='footer__logo'>noui0x</a>
+      <ul className='permalinks'>
+        <li><a href=''>Home</a></li>
+        <li><a href='#about'>About</a></li>
+        <li><a href='#experience'>Expeirience</a></li>
+        <li><a href='#services'>Services</a></li>
+        <li><a href='#portfolio'>Portfolio</a></li>
+        <li><a href='#testimonials'>Testimonials</a></li>
+        <li><a href='#contact'>Contact</a></li>
+      </ul>
+      <div className='footer__socials'>
+        <a href=''><FaFacebookF/></a>
+        <a href=''><FaInstagramSquare/></a>
+        <a href=''><FaLinkedin/></a>
+      </div>
+      <div className='footer__copyright'>
+        <small>© noui0x  All Rights Reserved</small>
+      </div>
+    </footer>
   )
 }
 
