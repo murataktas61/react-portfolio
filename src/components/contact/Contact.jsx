2fcbbafb53e5856fc4913cd6a932fd6e5edb6b9a
noui61 <noui@metavault.org>
2022-07-22 15:53:52 +0300

    testimonials and contact section update

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,54 @@
 import React from 'react'
 import './contact.css'
+import {MdOutlineEmail} from 'react-icons/md'
+import {RiMessengerLine} from 'react-icons/ri'
+import {BsWhatsapp} from 'react-icons/bs'
+import { useRef } from 'react';
+import emailjs from 'emailjs-com'
+
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_rwibnyl', 'template_ntskyyv', form.current, 'Xabrc8HKt_QuhUbxa')
+    e.target.reset()
+  };
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get ın Touch</h5>
+      <h2>Contact Me</h2>
+      <div className='container contact__container'>
+        <div className='contact_options'>
+          <article className="contact__option">
+            <MdOutlineEmail className='contact__option-icon'/>
+            <h4>Email</h4>
+            <h5>aktasmuratt7@gmail.com</h5>
+            <a href='mailto:aktasmuratt7@gmail.com' target="_blank">Send a message</a>
+          </article> <br></br>
+          <article className="contact__option">
+            <RiMessengerLine className='contact__option-icon'/>
+            <h4>Messenger</h4>
+            <h5>noui0x</h5>
+            <a href='https://m.me/murat.aktas19' target="_blank">Send a message</a>
+          </article>  <br></br>
+          <article className="contact__option">
+            <BsWhatsapp className='contact__option-icon'/>
+            <h4>WhatsApp</h4>
+            <h5>5378523995</h5>
+            <a href='https://wa.me/905378523995' target="_blank">Send a message</a>
+          </article>
+        </div>
+        <form ref={form} onSubmit={sendEmail}>
+        <input  type="text" name='name' placeholder='Your Full Name' required />
+        <input  type="email" name='email' placeholder='Your Email' required />
+        <textarea  name='message' placeholder='Your  Message' required />
+        <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,78 @@
 import React from 'react'
 import './testimonials.css'
+import AVTR1 from '../../assets/avatar1.jpg'
+import AVTR2 from '../../assets/avatar2.jpg'
+import AVTR3 from '../../assets/avatar3.jpg'
+import AVTR4 from '../../assets/avatar4.jpg'
+
+import { Pagination } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+
+const data=
+[
+  {
+    id:1,
+    avatar:AVTR1,
+    name: 'Tina Snow',
+    review:'Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan mıgır metinlerdir. Lorem Ipsum, adı bilinmeyen bir matbaacının bir hurufat numune kitabı oluşturmak üzere bir yazı galerisini alarak karıştırdığı 1500 lerden beri endüstri standardı sahte metinler olarak kullanılmıştır.'
+  },
+  {
+    id:2,
+    avatar:AVTR2,
+    name: 'Shatta Wale',
+    review:'Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan mıgır metinlerdir. Lorem Ipsum, adı bilinmeyen bir matbaacının bir hurufat numune kitabı oluşturmak üzere bir yazı galerisini alarak karıştırdığı 1500 lerden beri endüstri standardı sahte metinler olarak kullanılmıştır.'
+  },
+  {
+    id:3,
+    avatar:AVTR3,
+    name: 'Kwame Despite',
+    review:'Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan mıgır metinlerdir. Lorem Ipsum, adı bilinmeyen bir matbaacının bir hurufat numune kitabı oluşturmak üzere bir yazı galerisini alarak karıştırdığı 1500 lerden beri endüstri standardı sahte metinler olarak kullanılmıştır.'
+  },
+  {
+    id:3,
+    avatar:AVTR4,
+    name: 'Kwame Despite',
+    review:'Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan mıgır metinlerdir. Lorem Ipsum, adı bilinmeyen bir matbaacının bir hurufat numune kitabı oluşturmak üzere bir yazı galerisini alarak karıştırdığı 1500 lerden beri endüstri standardı sahte metinler olarak kullanılmıştır.'
+  },
+]
+
 
 const Testimonials = () => {
   return (
-    <section id='testimonials'>Testimonials</section>
+    <section id='testimonials'>
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
+      <Swiper className='container testimonials__container'
+           modules={[Pagination]}
+           spaceBetween={40}
+           slidesPerView={1}
+           pagination={{ clickable: true }}
+           scrollbar={{ draggable: true }}
+           onSwiper={(swiper) => console.log(swiper)}
+           onSlideChange={() => console.log('slide change')}
+      >
+        {
+          data.map(({id,avatar,name,review})=>{
+            return(
+              <SwiperSlide key={id} className='testimonial'>
+              <div className="client__avatar">
+                <img src={avatar} alt='Avatar One' />
+               
+              </div>
+              <h5 className='client__name'>{name}</h5>
+                <small className='client__review'>{review}</small>
+            </SwiperSlide>
+            )
+          })
+        }
+      </Swiper>
+    </section>
   )
 }
 
